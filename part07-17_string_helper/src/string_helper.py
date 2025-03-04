a9ff56a356a8ff099e2fc25a7ee0912b084e353f
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-03-04 23:27:50 +0000

    part07-17_string_helper | split_in_half()

--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -13,7 +13,16 @@ def change_case(orig_string: str) -> str:
     
     return torned_string
 
+def split_in_half(orig_string: str) -> tuple:
+    half = int(len(orig_string) / 2) # equals 8
+
+    p1 = orig_string[0:half]
+    p2 = orig_string[half:]
+    
+    return (p1, p2)
 
 if __name__ == "__main__":
     #Test functions
-    print(change_case("Well hello there!"))
+    string = "Well hello there!"
+    print(change_case(string))
+    print(split_in_half(string))
