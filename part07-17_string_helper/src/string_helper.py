03cdad3654742cf21b7c4126fca9079c5af8ce35
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-03-04 23:37:58 +0000

    part07-17_string_helper | remove_special_characters()

--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -21,8 +21,19 @@ def split_in_half(orig_string: str) -> tuple:
     
     return (p1, p2)
 
+def remove_special_characters(orig_string: str):
+    no_characters: str = ""
+
+    for letter in orig_string:
+        if letter.isalnum() or letter ==" ":
+            no_characters += letter
+            
+    return no_characters
+
+
 if __name__ == "__main__":
     #Test functions
     string = "Well hello there!"
     print(change_case(string))
     print(split_in_half(string))
+    print(remove_special_characters("This is a test, lets see how it goes!!!11!"))
