635e7b20124a223d18182382b6431499efbe2469
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-03-04 22:49:22 +0000

    part07-17_string_helper | change_case

--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,19 @@
 # Write your solution here
+
+def change_case(orig_string: str) -> str:
+    torned_string: str = ""
+    
+    for letter in orig_string:
+        if letter.islower():
+            torned_string += letter.upper()
+        elif letter.isupper():
+            torned_string += letter.lower()
+        else:
+            torned_string += letter
+    
+    return torned_string
+
+
+if __name__ == "__main__":
+    #Test functions
+    print(change_case("Well hello there!"))
