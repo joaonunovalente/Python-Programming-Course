0f624d5802c92966027596b1fee5aaec8cba2638
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-03-09 23:51:59 +0000

    part07-18_own_programming_language | Created a dict with all variables set to 0

--- part07-18_own_programming_language/src/own_language.py
+++ part07-18_own_programming_language/src/own_language.py
@@ -1,1 +1,24 @@
+import string
+
 # Write your solution here
+def run(program: list) -> list:
+    output_list: list = []
+
+    # Dictionary with all variables
+    aDict = dict.fromkeys(string.ascii_uppercase, 0)
+    print(aDict)
+
+
+    return output_list
+
+if __name__ == "__main__":
+    program1 = []
+    program1.append("MOV A 1")
+    program1.append("MOV B 2")
+    program1.append("PRINT A")
+    program1.append("PRINT B")
+    program1.append("ADD A B")
+    program1.append("PRINT A")
+    program1.append("END")
+    result = run(program1)
+    print(result)
