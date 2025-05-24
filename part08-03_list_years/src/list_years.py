ced53ef73d439ac36fb5727f5e7a051010549770
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-05-24 23:41:45 +0100

    Part 8 - Exercise 3

--- part08-03_list_years/src/list_years.py
+++ part08-03_list_years/src/list_years.py
@@ -1,3 +1,20 @@
 # Write your solution here
 # Remember the import statement
-# from datetime import date
+from datetime import date
+
+def list_years(dates: list) -> list:
+    list_years_ordered: list = []
+    for date in dates:
+        list_years_ordered.append(date.year)
+    list_years_ordered.sort()
+
+    return list_years_ordered
+
+
+if __name__ == "__main__":
+    date1 = date(2019, 2, 3)
+    date2 = date(2006, 10, 10)
+    date3 = date(1993, 5, 9)
+
+    years = list_years([date1, date2, date3])
+    print(years)
