32a262f001469292f748931cd3ea1faeb181c370
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-05-28 17:00:22 +0100

    Part 8 - Exercise 15

--- part08-15_lunchcard/src/lunchcard.py
+++ part08-15_lunchcard/src/lunchcard.py
@@ -1,1 +1,38 @@
-# Write your solution here:
+class LunchCard():
+    def __init__(self,  balance: float):
+        self.balance: float = balance
+
+    def eat_lunch(self):
+        if self.balance >= 2.60:
+            self.balance -= 2.60
+    
+    def eat_special(self):
+        if self.balance >= 4.60:
+            self.balance -= 4.60
+
+    def deposit_money(self, money_added: int):
+        if money_added <= 0:
+            raise ValueError
+        else:
+            self.balance += money_added
+
+    def __str__(self):
+        return f"The balance is {self.balance:.1f} euros"
+
+peters_card = LunchCard(20)
+graces_card = LunchCard(30)
+peters_card.eat_special()
+graces_card.eat_lunch()
+print("Peter: " + f"The balance is {peters_card.balance:.1f} euros")
+print("Grace: " + f"The balance is {graces_card.balance:.1f} euros")
+
+peters_card.deposit_money(20)
+graces_card.eat_special()
+print("Peter: " + f"The balance is {peters_card.balance:.1f} euros")
+print("Grace: " + f"The balance is {graces_card.balance:.1f} euros")
+
+peters_card.eat_lunch()
+peters_card.eat_lunch()
+graces_card.deposit_money(50)
+print("Peter: " + f"The balance is {peters_card.balance:.1f} euros")
+print("Grace: " + f"The balance is {graces_card.balance:.1f} euros")
