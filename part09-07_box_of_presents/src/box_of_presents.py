b83d821d0d48844042bd2cbf53c3a451cfa7dd0f
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-06-29 15:29:41 +0100

    Part 9 - Exercise 7

--- part09-07_box_of_presents/src/box_of_presents.py
+++ part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,31 @@
-# WRITE YOUR SOLUTION HERE:
+class Present:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+    
+    def __str__(self):
+        return f"Present: {book.name} ({book.weight}kg)"
+    
+class Box:
+    def __init__(self):
+        self.box = []
+
+    def add_present(self, present: Present):
+        self.box.append(present)
+
+    def total_weight(self):
+        total_weight = 0
+        for present in self.box:
+            total_weight += present.weight
+        return total_weight
+
+if __name__ == "__main__":
+    book = Present("ABC Book", 2)
+
+    box = Box()
+    box.add_present(book)
+    print(box.total_weight())
+
+    cd = Present("Pink Floyd: Dark Side of the Moon", 1)
+    box.add_present(cd)
+    print(box.total_weight())
