ecfc8cf1cbefe4e5af2ec2d27217af91815ceac8
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-06-29 20:47:07 +0100

    Part 9 - Exercise 10

--- part09-10_recording/src/recording.py
+++ part09-10_recording/src/recording.py
@@ -1,1 +1,24 @@
-# WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length: int):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("Error")
+
+    @property
+    def length(self):
+        return self.__length
+    
+    @length.setter
+    def length(self, length):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("Error")
+           
+     
+if __name__ == "__main__":
+    the_wall = Recording(43)
+    print(the_wall.length)
+    the_wall.length = 22
+    print(the_wall.length)
