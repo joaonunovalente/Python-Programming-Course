858e817eb49133b6bb7d9a58db3bf41b0489c40d
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-06-29 21:28:34 +0100

    Part 9 - Exercise 11

--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,23 @@
-# WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, station: str):
+        self.__station: str = station
+        self.__observations: list = []
+    
+    def add_observation(self, observation: str):
+        self.__observations.append(observation)
+    
+    def latest_observation(self):
+        if not self.__observations:
+            return ""
+        return self.__observations[-1]
+    
+    def number_of_observations(self):
+        return len(self.__observations)
+    
+    def __str__(self):
+        return f"{self.__station}, {self.number_of_observations()} observations"
 
 
+if __name__ == "__main__":
+    a=WeatherStation("Kumpula")
+    m=a.latest_observation()
