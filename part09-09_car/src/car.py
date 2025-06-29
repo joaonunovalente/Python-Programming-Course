77fdfbb756b4f1257ea8125ac2f20dff5acf192f
João Nuno Valente <39313388+joaonunovalente@users.noreply.github.com>
2025-06-29 20:25:38 +0100

    Part 9 - Exercise 9

--- part09-09_car/src/car.py
+++ part09-09_car/src/car.py
@@ -1,1 +1,33 @@
-# WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__amout_of_petrol: int = 0
+        self.__odometer: int = 0
+
+    def fill_up(self):
+        self.__amout_of_petrol = 60
+        
+    def drive(self, km: int):
+        if self.__amout_of_petrol > km:
+            self.__odometer += km
+            self.__amout_of_petrol -= km
+        else:
+            self.__odometer += self.__amout_of_petrol
+            self.__amout_of_petrol = 0
+    
+    def __str__(self):
+        return f"Car: odometer reading {self.__odometer} km, petrol remaining {self.__amout_of_petrol} litres"
+
+if __name__ == "__main__":
+    car = Car()
+    print(car)
+    car.fill_up()
+    print(car)
+    car.drive(20)
+    print(car)
+    car.drive(50)
+    print(car)
+    car.drive(10)
+    print(car)
+    car.fill_up()
+    car.fill_up()
+    print(car)
