9913a80fc66314641df28a8e442bf6e706913ba8
João Nuno Valente <jnvalente@ua.pt>
2025-07-04 06:02:12 +0100

    Part 10 - Exercise 3

--- part10-03_areas/src/areas.py
+++ part10-03_areas/src/areas.py
@@ -9,3 +9,19 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
+
+class Square(Rectangle):
+    def __init__(self, width: int):
+        super().__init__(width, width)
+
+    def __str__(self):
+        return f"square {self.width}x{self.width}"
+    
+    def area(self):
+        area = super().area()
+        return area
+    
+if __name__ == "__main__":
+    square = Square(4)
+    print(square)
+    print("area:", square.area())
