c6128a66633d7bff74e9e26d054247a40cd0ecc3
João Nuno Valente <jnvalente@ua.pt>
2025-07-12 12:10:17 +0100

    Part 11 - Exercise 9

--- part11-08_filter_forbidden/src/filter_forbidden.py
+++ part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,5 +1,4 @@
 def filter_forbidden(string: str, forbidden: str):
-    # return [character for character in [word for word in string.split()]]
     return "".join([character for character in string if character not in forbidden])
 
 if __name__ == "__main__":
