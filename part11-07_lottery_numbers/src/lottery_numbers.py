4f50c4922f83f0c486da960253cb7aef7c5278f7
João Nuno Valente <jnvalente@ua.pt>
2025-07-12 01:08:36 +0100

    Part 11 - Exercise 7

--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,16 @@
-# WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week: int, numbers: int):
+        self.week = week
+        self.correct_numbers = numbers
+    
+    def number_of_hits(self, hit_numbers: int):
+        return len([number for number in self.correct_numbers if number in hit_numbers])
+    
+    def hits_in_place(self, hit_numbers):
+        return [number if number in self.correct_numbers else -1 for number in hit_numbers]
+
+if __name__ == "__main__":
+    week8 = LotteryNumbers(8, [1,2,3,10,20,30,33])
+    my_numbers = [1,4,7,10,11,20,30]
+
+    print(week8.hits_in_place(my_numbers))
