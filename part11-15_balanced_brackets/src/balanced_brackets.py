8fb4a4fffea430213b3556db55e79788b9a982a9
João Nuno Valente <jnvalente@ua.pt>
2025-07-12 22:11:23 +0100

    Part 11 - Exercise 15

--- part11-15_balanced_brackets/src/balanced_brackets.py
+++ part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,9 +1,34 @@
-
 def balanced_brackets(my_string: str):
+    my_string = "".join([character for character in my_string if character in "()[]"])
     if len(my_string) == 0:
-        return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
+        return True    
+
+    if (not (my_string[0] == '(' and my_string[-1] == ')')) and  (not (my_string[0] == '[' and my_string[-1] == ']')):
         return False
 
     # remove first and last character
     return balanced_brackets(my_string[1:-1])
+
+if __name__ == "__main__":
+    ok = balanced_brackets("(((())))")
+    print(ok)
+
+    ok = balanced_brackets("([([])])")
+    print(ok)
+
+    ok = balanced_brackets("(python version [3.7]) please use this one!")
+    print(ok)
+
+    # this is no good, the closing bracket doesn't match
+    ok = balanced_brackets("(()]")
+    print(ok)
+
+    # different types of brackets are mismatched
+    ok = balanced_brackets("([bad egg)]")
+    print(ok)
+
+    ok = balanced_brackets("(hello)")
+    print(ok)
+    
+    ok = balanced_brackets("hello")
+    print(ok)
