80ff633de0da4daad78e4295969f0ed8f8f27244
João Nuno Valente <jnvalente@ua.pt>
2025-07-12 12:55:03 +0100

    Part 11 - Exercise 11

--- part11-11_lengths_of_strings/src/lengths_of_strings.py
+++ part11-11_lengths_of_strings/src/lengths_of_strings.py
@@ -1,1 +1,9 @@
 # WRITE YOUR SOLUTION HERE:
+def lengths(word_list: list):
+    return {word: len(word) for word in word_list}
+
+if __name__ =="__main__":
+    word_list = ["once", "upon" , "a", "time", "in"]
+
+    word_lengths = lengths(word_list)
+    print(word_lengths)
