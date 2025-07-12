c3962d09cbadd9a3d0900d7e2273598b23d6b472
João Nuno Valente <jnvalente@ua.pt>
2025-07-12 01:55:25 +0100

    Part 11 - Exercise 8

--- part11-08_filter_forbidden/src/filter_forbidden.py
+++ part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,1 +1,8 @@
-# WRITE YOUR SOLUTION HERE:
+def filter_forbidden(string: str, forbidden: str):
+    # return [character for character in [word for word in string.split()]]
+    return "".join([character for character in string if character not in forbidden])
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
