3b72ec98115d4b187ef99acc240d9ccf1a5334fd
João Nuno Valente <jnvalente@ua.pt>
2025-07-12 14:37:13 +0100

    Part 11 - Exercise 12

--- part11-12_most_common_words/src/most_common_words.py
+++ part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,14 @@
-# WRITE YOUR SOLUTION HERE:
+def read_file(filename: str):
+    with open(filename) as new_file:
+        contents = new_file.read()
+    
+    return contents
+
+def most_common_words(filename: str, lower_limit: int):
+    contents = read_file(filename)
+    contents = "".join([character for character in contents if character not in ".!,?:"])
+    splitted  = {word: contents.split().count(word) for word in contents.split() if contents.split().count(word) >= lower_limit}
+    return splitted
+
+if __name__ == "__main__":
+    print(most_common_words("comprehensions.txt", 3))
