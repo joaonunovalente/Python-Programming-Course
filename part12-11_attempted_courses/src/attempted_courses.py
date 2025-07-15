11988dea18105693e53dc6b5588c657ed0a3492f
João Nuno Valente <jnvalente@ua.pt>
2025-07-15 23:02:34 +0100

    Part 12 - Exercise 11

--- part12-11_attempted_courses/src/attempted_courses.py
+++ part12-11_attempted_courses/src/attempted_courses.py
@@ -6,5 +6,17 @@ class CourseAttempt:
 
     def __str__(self):
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+    
+def names_of_students(attempts: list):
+    return list(map(lambda student: student.student_name, attempts))
 
-# Write your solution here
+def course_names(attempts: list):
+    return set(list(map(lambda student: student.course_name, list(sorted(attempts, key=lambda student: student.course_name)))))
+
+if __name__ == "__main__":
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+
+    for name in course_names([s1, s2, s3]):
+        print(name)
