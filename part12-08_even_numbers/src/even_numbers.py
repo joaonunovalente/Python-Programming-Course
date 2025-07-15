4a71d23ddeb81e7456058d23a532496e9465b0c0
João Nuno Valente <jnvalente@ua.pt>
2025-07-15 13:30:41 +0100

    Part 12 - Exercise 8

--- part12-08_even_numbers/src/even_numbers.py
+++ part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    print("hello")
+    if beginning % 2 == 1:
+        number = beginning + 1
+    else:
+        number = beginning
+
+    while number <= maximum:
+        yield number
+        number += 2
+        
+
+if __name__ == "__main__":
+    numbers = even_numbers(2, 10)
+    for number in numbers:
+        print(number)
