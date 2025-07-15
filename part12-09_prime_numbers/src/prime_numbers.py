ee9fce13aada4c9d15998b8a76360b3fc4562fb5
João Nuno Valente <jnvalente@ua.pt>
2025-07-15 13:55:17 +0100

    Part 12 - Exercise 9

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,24 @@
-# Write your solution here
+from math import sqrt
+
+def is_prime(n, i=None):
+    if n <= 1:
+        return False
+    if i is None:
+        i = int(sqrt(n))
+    if i == 1:
+        return True
+    if n % i == 0:
+        return False
+    return is_prime(n, i - 1)
+
+def prime_numbers():
+    number = 2
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+
+if __name__ == "__main__":
+    numbers = prime_numbers()
+    for _ in range(8):
+        print(next(numbers))
