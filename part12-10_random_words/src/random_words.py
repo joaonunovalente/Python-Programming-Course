6ebb6e63e9d9b4bbcf85274f3505041dd0332071
João Nuno Valente <jnvalente@ua.pt>
2025-07-15 17:48:50 +0100

    Part 12 - Exercise 10

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -1,1 +1,9 @@
-# Write your solution here:
+def word_generator(characters: str, length: int, amount: int):
+    substrings = (characters[i : i + length] for i in range(amount))
+
+    return substrings
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
