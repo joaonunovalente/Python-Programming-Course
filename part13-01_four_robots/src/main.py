5a0144b290342bc3518295f038a6226ad8dea038
João Nuno Valente <jnvalente@ua.pt>
2025-07-18 15:19:06 +0100

    Part 13 - Exercise 1

--- part13-01_four_robots/src/main.py
+++ part13-01_four_robots/src/main.py
@@ -7,3 +7,27 @@
 
 # WRITE YOUR SOLUTION HERE:
 
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+window.fill((0, 0, 0))
+
+
+robot = pygame.image.load("robot.png")
+robot_width = robot.get_width()
+robot_height = robot.get_height()
+
+
+window.blit(robot, (0, 0))
+window.blit(robot, (0, 480 - robot_height))
+window.blit(robot, (640 - robot_width, 0))
+window.blit(robot, (640 - robot_width, 480 - robot_height))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
