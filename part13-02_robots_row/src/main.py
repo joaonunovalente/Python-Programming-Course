b2e3eb8bb8aee82b445be020adeaa474196a2c3d
João Nuno Valente <jnvalente@ua.pt>
2025-07-20 00:24:03 +0100

    Part 13 - Exercise 2

--- part13-02_robots_row/src/main.py
+++ part13-02_robots_row/src/main.py
@@ -1,1 +1,26 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+window.fill((0, 0, 0))
+
+
+robot = pygame.image.load("robot.png")
+robot_width = robot.get_width()
+robot_height = robot.get_height()
+
+height = 100
+for i in range(0, 10):
+    width = 50 + i * 10
+    for ii in range(0,10):
+        window.blit(robot, (width, height))
+        width += 40
+    height += 20
+    
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
