43d0cd719c28696566c50234aebdd39d557b451e
João Nuno Valente <jnvalente@ua.pt>
2025-07-20 00:38:31 +0100

    Part 13 -  Exercise 3

--- part13-03_hundred_robots/src/main.py
+++ part13-03_hundred_robots/src/main.py
@@ -1,1 +1,25 @@
-# WRITE YOUR SOLUTION HERE:
+from random import randint
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+window.fill((0, 0, 0))
+
+
+robot = pygame.image.load("robot.png")
+robot_width = robot.get_width()
+robot_height = robot.get_height()
+
+for i in range(0,1000):
+    x = randint(0, 590)
+    y = randint(0, 390)
+    window.blit(robot, (x, y))
+
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
